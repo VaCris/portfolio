fdc77b512698ffaa818cf913d1505c2182a7bca0
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-08-16 13:22:35 -0500

    fix(config): add missing remote pattern for CDN images

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,13 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.ibb.co",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
